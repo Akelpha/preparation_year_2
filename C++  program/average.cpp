4f1ecf689a5329aa9d0a8c5ae3c4108894a4e744
Keren Nzira <kerennzira@gmail.com>
2026-09-14 22:26:54 +0100

    Add average and essaie programs with basic functionality

---
+++ C++  program/average.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name = "Amine";
+    int note1 = 14;
+    int note2 = 16;
+    double moyenne = (note1+note2)/2;
+
+    cout << "Etudiant: " <<name << endl;
+    cout << "Moyenne: " <<moyenne << endl;
+
+    return 0;
+    
+}
---
+++ C++  program/essaie.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+
+ int main () {
+
+ cout << " Bienvenue a l’UEMF !" << endl ;
+ cout << " Programmation C++" << endl ;
+
+ return 0;
+ }
