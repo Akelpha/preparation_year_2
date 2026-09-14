5d561d6b20c21d22390c59e8961035cb664acbe8
Keren Nzira <kerennzira@gmail.com>
2026-09-14 21:43:46 +0100

    Refactor code structure for improved readability and maintainability

---
+++ C++  program/age.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 19;
+     cout << "Bienvenue à L'UEMF!"<< endl;
+     cout << "Age " <<age <<" ans" <<endl;
+     return 0;
+}
