34d86528627a22832618c5a45ebcdd12ff8eea32
akelpha <kerennzira@gmail.com>
2026-09-15 11:35:44 +0100

    Refactor code formatting for consistency and readability across multiple C++ files

---
+++ C++  program/identity.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nom = "Nzira";
+    string prenom = "Keren";
+    string filiere = "EIDIA";
+
+    cout << "Votre Nom: " << nom << endl;
+    cout << "Votre prenom: "<< prenom <<endl;
+    cout << "Votre filiere : "<<filiere<<endl;
+
+    return 0;
+
+}
